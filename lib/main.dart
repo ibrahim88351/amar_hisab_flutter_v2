42cba26c89112ec7a8f1a985ff1c08a8b72d0d9e
ibrahim88351 <113986149+ibrahim88351@users.noreply.github.com>
2025-05-27 10:49:45 +0600

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'আমার হিসাব',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('আমার হিসাব')),
+      body: Center(child: Text('হিসাব লিখুন', style: TextStyle(fontSize: 20))),
+    );
+  }
+}
